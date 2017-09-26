3df4ef4971250bdf34617f4f1d42a60af0b2ddbf
KiwiLss <m15205622327@163.com>
2017-09-26 15:19:55 +0800

    9-26

---
+++ app/src/main/java/com/winding/easyhttptest/adapter/DialogAdapter.java
@@ -0,0 +1,23 @@
+package com.winding.easyhttptest.adapter;
+
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+import java.util.List;
+
+/**
+ * Created by 刘少帅 on 2017/9/26
+ */
+
+public class DialogAdapter extends BaseQuickAdapter<String,BaseViewHolder> {
+    public DialogAdapter(@LayoutRes int layoutResId, @Nullable List<String> data) {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, String item) {
+    }
+}
--- app/src/main/java/com/winding/easyhttptest/ui/DialogActivity.java
+++ app/src/main/java/com/winding/easyhttptest/ui/DialogActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.kiwi.library.PopupOneUtils;
+import com.kiwi.library.PopupOneUtils2;
 import com.winding.easyhttptest.R;
 import com.winding.easyhttptest.manage.DialogManage;
 
@@ -19,13 +21,45 @@ public class DialogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dialog);
         //DialogManage.getInstance().show(this,"玩命加载中....");
         DialogManage.getInstance().showLoading(this);
+
+
+
+
+
     }
 
     public void showProgressDialog(View view) {
-        DialogManage.getInstance().show(this,"显示一下....");
+
+        new PopupOneUtils2(this, new PopupOneUtils2.ContentClickListener() {
+            @Override
+            public void sureClickListener(PopupOneUtils2 popupOneUtils2) {
+                popupOneUtils2.dismiss();
+            }
+
+            @Override
+            public void cancelClickListener() {
+
+            }
+        }).setTitleAndColor("小小试试",0,true)
+                .showCenter(this);
+
     }
 
-    public void dimiProgressDialog(View view) {
-        DialogManage.getInstance().dimiss();
+
+
+    public void dimissProgressDialog(View view) {
+        new PopupOneUtils(this, new PopupOneUtils.ContentClickListener() {
+            @Override
+            public void sureClickListener(PopupOneUtils popupOneUtils) {
+
+            }
+
+            @Override
+            public void cancelClickListener() {
+
+            }
+        }).setTitleAndColor("hellokitty",0,true)
+                .setRadius(2)
+                .showCenter(this);
     }
 }
